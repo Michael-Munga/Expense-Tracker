e66c883e55088b71209e083a47d41365658a6a59
Michael Munga <micknyalle@gmail.com>
2025-04-14 21:00:13 +0300

    Create basic structure for ExpenseForm, ExpenseList, and SearchBar components

---
+++ expense-tracker/src/Components/ExpenseForm.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ExpenseForm() {
+  return (
+    <div>
+      <h2>Expense Form</h2>
+    </div>
+  );
+}
---
+++ expense-tracker/src/Components/ExpenseList.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ExpenseList() {
+  return (
+    <div>
+      <h2>Expense List</h2>
+    </div>
+  );
+}
