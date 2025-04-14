2ca344cac1cc40a52a6e0a12fde83729f42c2b96
Michael Munga <micknyalle@gmail.com>
2025-04-14 21:10:16 +0300

    Render ExpenseForm, ExpenseList, and SearchBar in App component

--- expense-tracker/src/App.jsx
+++ expense-tracker/src/App.jsx
@@ -1,7 +1,19 @@
+import React from "react";
+import ExpenseForm from "./Components/ExpenseForm";
+import ExpenseList from "./Components/ExpenseList";
+import SearchBar from "./Components/SearchBar";
+
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Expense Tracker</h1>
+      <SearchBar />
+      <ExpenseForm />
+      <ExpenseList />
+    </div>
+  );
 }
 
 export default App;
